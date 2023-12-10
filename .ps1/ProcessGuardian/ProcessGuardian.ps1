1643910db9e23799dfafbb5abf57339fb778ff54
Raydo Matthee <126121348+burnt-exe@users.noreply.github.com>
2023-12-10 10:18:56 +0200

    Update ProcessGuardian.ps1
    Signed-off-by: Raydo Matthee <126121348+burnt-exe@users.noreply.github.com>

--- .ps1/ProcessGuardian/ProcessGuardian.ps1
+++ .ps1/ProcessGuardian/ProcessGuardian.ps1
@@ -15,7 +15,34 @@ This script does not collect or transmit any personal or sensitive data. It oper
 This tool is intended for use by experienced IT professionals. Incorrect use of this tool may lead to unintended system disruptions or network issues. Use it at your own risk. The author or organization is not responsible for any damage or data loss caused by the use of this script.
 
 .LINK
-GitHub: https://www.github.com/skunkworks.africa
+GitHub: https://github.com/skunkworksza/microsoft/tree/main/.ps1/ProcessGuardian
+
+MANUAL
+    NAME
+        ProcessGuardian.ps1 - A tool for managing and investigating system processes.
+
+    SYNOPSIS
+        ./ProcessGuardian.ps1
+
+    DESCRIPTION
+        ProcessGuardian is a PowerShell script for IT professionals to manage system processes. 
+        It provides options to stop and blacklist remote access processes, log activities, 
+        list irregular processes for investigation, and display blacklisted processes.
+
+    OPTIONS
+        1. Stop all and add to blacklist
+        2. Kill process if all entries listed
+        3. List irregular processes for investigation
+        4. Display blacklisted processes
+
+    REPORTING BUGS
+        Report bugs to the GitHub Issues page at <https://github.com/skunkworksza/microsoft/issues>.
+
+    COPYRIGHT
+        Copyright (c) 2023 Skunkworks EdTech. License MIT: The MIT License <https://opensource.org/licenses/MIT>.
+
+    SEE ALSO
+        GitHub Repository: <https://github.com/skunkworksza/microsoft/tree/main/.ps1/ProcessGuardian>
 #>
 
 # PowerShell script to manage remote access processes
