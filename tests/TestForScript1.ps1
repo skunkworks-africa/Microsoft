ceeebad9b76c8c89b7635bcf26238ffb30b3f503
Raydo Matthee <126121348+burnt-exe@users.noreply.github.com>
2023-12-10 23:25:43 +0200

    Create TestForScript1.ps1
    Signed-off-by: Raydo Matthee <126121348+burnt-exe@users.noreply.github.com>

---
+++ tests/TestForScript1.ps1
@@ -0,0 +1,13 @@
+# TestForScript1.ps1
+
+Describe "Script1 Tests" {
+    Context "Functionality of Script1" {
+        It "Does something expected" {
+            # Setup for the test
+            # Example: $result = .\Script1.ps1 -SomeParameter "Test"
+            
+            # The assertion
+            # Example: $result | Should -Be "ExpectedResult"
+        }
+    }
+}
